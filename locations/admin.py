ed270063c914861f865fa3e407c30b42fba5bfef
mrisis <rezaamin8889@gmail.com>
2025-01-30 09:17:33 +0330

    write City model & admin

--- locations/admin.py
+++ locations/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Province
+from .models import Province, City
 
 
 class ProvinceAdmin(admin.ModelAdmin):
@@ -8,4 +8,13 @@ class ProvinceAdmin(admin.ModelAdmin):
     ordering = ('name',)
 
 
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'province')
+    list_filter = ('province__name',)
+    search_fields = ('name', 'province__name')
+    ordering = ('name',)
+
+
 admin.site.register(Province, ProvinceAdmin)
+admin.site.register(City, CityAdmin)
+
--- locations/models.py
+++ locations/models.py
@@ -12,3 +12,13 @@ class Province(BaseModel):
     class Meta:
         ordering = ['name']
 
+
+class City(BaseModel):
+    name = models.CharField(max_length=255)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='cities')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
