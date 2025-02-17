e6433c5f5caa00d489b960d6a9006b3a2b11831c
mrisis <rezaamin8889@gmail.com>
2025-02-17 13:31:58 +0330

    add field icon in Role model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -113,6 +113,11 @@ class Role(BaseModel):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
     category = models.ForeignKey(RoleCategory, on_delete=models.CASCADE, related_name='roles')
+    icon = models.FileField(
+        upload_to='roles/icons/',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.name
