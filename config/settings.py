3d78a21200eebf8d576363c3e9be500cad662350
mrisis <rezaamin8889@gmail.com>
2025-02-19 19:15:27 +0330

    add media url & root in urls.py and change in settings.py

--- config/settings.py
+++ config/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = env.media_root
 STATIC_ROOT = env.static_root
 
